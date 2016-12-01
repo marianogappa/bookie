27ffa246bce2e3c19842d89fa50607f06c0cf506
GaryZhu <Gary@movio.co>
2016-12-01 17:39:47 +1300

    updates sqls

--- mariadb.sql
+++ mariadb.sql
@@ -8,15 +8,15 @@ CREATE TABLE IF NOT EXISTS bookie.scrape (
     updated datetime DEFAULT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY uniqueScrape (topic, topic_partition)
-) ENGINE=InnoDB AUTO_INCREMENT=2627 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS bookie.fsm (
     fsmID varchar(100) NOT NULL,
     created datetime DEFAULT NULL,
-    PRIMARY KEY (fsmID),
-) ENGINE=InnoDB AUTO_INCREMENT=2627 DEFAULT CHARSET=utf8;
+    PRIMARY KEY (fsmID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS bookie.offset {
+CREATE TABLE IF NOT EXISTS bookie.offset (
     id int(11) NOT NULL AUTO_INCREMENT,
     fsmID varchar(100) NOT NULL,
 	topic varchar(100) NOT NULL,
@@ -27,13 +27,10 @@ CREATE TABLE IF NOT EXISTS bookie.offset {
     updated datetime DEFAULT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY uniqueScrapedFSM (fsm, topic, topic_partition)
-) ENGINE=InnoDB AUTO_INCREMENT=2627 DEFAULT CHARSET=utf8;
-}
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS bookie.fsmAliases (
-    id int(11) NOT NULL AUTO_INCREMENT,
     fsmID varchar(100) NOT NULL,
 	fsmAlias varchar(100) NOT NULL,
-	PRIMARY KEY (id),
-    UNIQUE KEY uniqueAliase (fsmID, fsmAlias)
-) ENGINE=InnoDB AUTO_INCREMENT=2627 DEFAULT CHARSET=utf8;
+	PRIMARY KEY (fsmID, fsmAlias)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
