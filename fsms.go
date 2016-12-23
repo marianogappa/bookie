ddaa648fe26bc5279694643bb92133455cb5fe23
Mariano Gappa <mariano@movio.co>
2016-12-23 20:55:11 +1300

    Implements fsms buffering and flushing.

---
+++ fsms.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"strconv"
+	"time"
+)
+
+type fsms struct {
+	t map[string]time.Time
+}
+
+func (f *fsms) add(fsmId string, created string, layout string) {
+	if f.t == nil {
+		f.t = map[string]time.Time{}
+	}
+
+	tm := f.parseTime(created, layout)
+
+	if _, ok := f.t[fsmId]; !ok || f.t[fsmId].After(tm) {
+		f.t[fsmId] = tm
+	}
+}
+
+func (f *fsms) flush() (string, []interface{}) {
+	vs := make([]interface{}, len(f.t)*2)
+	i := 0
+	for fsmId, tm := range f.t {
+		vs[i] = fsmId
+		i++
+		vs[i] = tm
+		i++
+	}
+
+	f.t = nil
+
+	return `INSERT INTO bookie.fsm(fsmID, created) VALUES ` +
+		buildInsertTuples(2, len(vs)/2) +
+		` ON DUPLICATE KEY UPDATE created = LEAST(created, VALUES(created))`, vs
+}
+
+func (f fsms) parseTime(t string, layout string) time.Time {
+	var err error
+	var _icreated int64
+	var created time.Time
+
+	if layout == "unix" {
+		_icreated, err = strconv.ParseInt(t, 10, 64)
+		created = time.Unix(_icreated, 0)
+	} else if layout == "unixNano" {
+		_icreated, err = strconv.ParseInt(t, 10, 64)
+		created = time.Unix(0, _icreated)
+	} else {
+		created, err = time.Parse(layout, t)
+	}
+
+	if err != nil {
+		return time.Time{}
+	}
+	return created
+}
---
+++ fsms_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFSMs(t *testing.T) {
+	fs := fsms{}
+	fs.add("16CCCB1C-BE16-4595-A610-6FC9E3F3CD80", "2006-01-02", "2006-01-02")
+	fs.add("4CFEE785-1AEC-421E-AC6F-8314BDAF43C8", "2006-03-04", "2006-01-02")
+	fs.add("EFD6E9BD-3888-4CA9-B0D3-7E57BA64F9D5", "2006-05-06", "2006-01-02")
+	sql, is := fs.flush()
+
+	assert.Equal(t, `INSERT INTO bookie.fsm(fsmID, created) VALUES (?,?),(?,?),(?,?) ON DUPLICATE KEY UPDATE created = LEAST(created, VALUES(created))`, sql, "sql mismatch")
+	assert.True(t, containsSubSlice(is, []interface{}{"16CCCB1C-BE16-4595-A610-6FC9E3F3CD80", timeParse("2006-01-02", "2006-01-02")}), "")
+	assert.True(t, containsSubSlice(is, []interface{}{"4CFEE785-1AEC-421E-AC6F-8314BDAF43C8", timeParse("2006-01-02", "2006-03-04")}), "")
+	assert.True(t, containsSubSlice(is, []interface{}{"EFD6E9BD-3888-4CA9-B0D3-7E57BA64F9D5", timeParse("2006-01-02", "2006-05-06")}), "")
+	assert.Equal(t, len(is), 6, "length mismatch")
+	assert.Nil(t, fs.t, "fsms was not cleared after flush")
+}
+
+func timeParse(l, s string) time.Time {
+	t, _ := time.Parse(l, s)
+	return t
+}
