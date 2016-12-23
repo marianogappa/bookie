89ebbd8f925f6b7a334ff1343a8b180de3392d9a
Mariano Gappa <mariano@movio.co>
2016-12-23 19:52:54 +1300

    Implements tags buffering and sql flusher.

---
+++ tags.go
@@ -0,0 +1,52 @@
+package main
+
+import "bytes"
+
+type tags struct {
+	t map[string]map[string]string
+}
+
+func (t *tags) add(fsmId string, k string, v string) {
+	if t.t == nil {
+		t.t = map[string]map[string]string{}
+	}
+
+	if _, ok := t.t[fsmId]; !ok {
+		t.t[fsmId] = map[string]string{k: v}
+		return
+	}
+	t.t[fsmId][k] = v
+}
+
+func (t *tags) flush() (string, []interface{}) {
+	vs := []interface{}{}
+	for fsmId, aux := range t.t {
+		for k, v := range aux {
+			vs = append(vs, fsmId, k, v)
+		}
+	}
+
+	t.t = nil
+
+	return `INSERT INTO bookie.tags(fsmID, k, v) VALUES ` +
+		t.buildInsertTuples(len(vs)/3, 3) +
+		` ON DUPLICATE KEY UPDATE v = VALUES(v)`, vs
+}
+
+func (t tags) buildInsertTuples(colN, rowN int) string {
+	s := bytes.NewBuffer([]byte{})
+	for i := 1; i <= rowN; i++ {
+		s.Write([]byte("("))
+		for j := 1; j <= colN; j++ {
+			s.Write([]byte("?"))
+			if j != colN {
+				s.Write([]byte(","))
+			}
+		}
+		s.Write([]byte(")"))
+		if i != rowN {
+			s.Write([]byte(","))
+		}
+	}
+	return s.String()
+}
