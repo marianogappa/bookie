f662c2c1f2fe0f1e66a39fbb3048b9836dca63c0
Mariano Gappa <mariano@movio.co>
2017-01-22 20:05:21 +1300

    Fixes incorrect expected string in test.

--- fsms_test.go
+++ fsms_test.go
@@ -15,7 +15,7 @@ func TestFSMs(t *testing.T) {
 	qs := fs.flush()
 
 	assert.Equal(t, 1, len(qs))
-	assert.Equal(t, `INSERT INTO bookie.fsm(fsmID, created) VALUES (?,?),(?,?),(?,?) ON DUPLICATE KEY UPDATE created = created = VALUES(created)`, qs[0].sql, "sql mismatch")
+	assert.Equal(t, `INSERT INTO bookie.fsm(fsmID, created) VALUES (?,?),(?,?),(?,?) ON DUPLICATE KEY UPDATE created = VALUES(created)`, qs[0].sql, "sql mismatch")
 	assert.True(t, containsSubSlice(qs[0].values, []interface{}{"16CCCB1C-BE16-4595-A610-6FC9E3F3CD80", timeParse("2006-01-02", "2006-01-02")}), "")
 	assert.True(t, containsSubSlice(qs[0].values, []interface{}{"4CFEE785-1AEC-421E-AC6F-8314BDAF43C8", timeParse("2006-01-02", "2006-03-04")}), "")
 	assert.True(t, containsSubSlice(qs[0].values, []interface{}{"EFD6E9BD-3888-4CA9-B0D3-7E57BA64F9D5", timeParse("2006-01-02", "2006-05-06")}), "")
